7507f86b9021ad9f4a25e94cf2b3b6da8bf0b376
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2012-05-23 13:45:14 +0300

    initiated repo

---
+++ src/componentprogramming/CompoLexical.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentprogramming;
+
+/**
+ *
+ * @author mhdsyrwan
+ */
+public class CompoLexical {
+    public class Token {
+    
+    }
+    
+    Token mCurrentToken;
+    void nextToken(){
+        //TODO add next token logic
+    }
+    Token currentToken(){
+        //TOOD add currnt Token Logic
+        return new Token();
+    }
+    
+    String ImageToken(){
+        //TODO return token string
+        return currentToken().toString();
+    }
+    
+}
